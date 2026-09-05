1b7978c225379f850f4093f9e474f461fd4115cf
Lucky gupta <guptalucky3692@gmail.com>
2026-09-05 19:39:53 +0530

    ✅ Mod Divmod [python3] — approach1

---
+++ solutions/python3/easy/python-mod-divmod/solution-approach1.py
@@ -0,0 +1,17 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-mod-divmod/problem?isFullScreen=true
+# Problem     Mod Divmod
+# Difficulty  Easy
+# Subdomain   Math
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-21, 04:31 p.m.
+# ──────────────────────────────────────────────────
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+print(a//b)
+print(a%b)
+print(divmod(a,b))
