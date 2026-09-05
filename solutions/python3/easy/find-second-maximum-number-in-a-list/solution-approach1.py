51411430944c0896ddd8adb52ccd21b37be5b0ae
Lucky gupta <guptalucky3692@gmail.com>
2026-09-05 19:31:37 +0530

    ✅ Find the Runner-Up Score!   [python3] — approach1

---
+++ solutions/python3/easy/find-second-maximum-number-in-a-list/solution-approach1.py
@@ -0,0 +1,16 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=true
+# Problem     Find the Runner-Up Score!  
+# Difficulty  Easy
+# Subdomain   Basic Data Types
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-14, 02:34 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    print(sorted(set(arr))[-2])
+    
