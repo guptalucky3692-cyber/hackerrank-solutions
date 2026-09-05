4b37cec5bc1beda6d5a6dec9c90a044a2a145803
Lucky gupta <guptalucky3692@gmail.com>
2026-09-05 19:32:40 +0530

    ✅ sWAP cASE [python3] — approach1

---
+++ solutions/python3/easy/swap-case/solution-approach1.py
@@ -0,0 +1,14 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/swap-case/problem?isFullScreen=true
+# Problem     sWAP cASE
+# Difficulty  Easy
+# Subdomain   Strings
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-21, 03:58 p.m.
+# ──────────────────────────────────────────────────
+
+def swap_case(s):
+    s = s.swapcase()
+    return s
