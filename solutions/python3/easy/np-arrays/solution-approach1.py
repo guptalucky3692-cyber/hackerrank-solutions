2d094e3badf1852f71ced0cefdd2ec8aa5e6e1b4
Lucky gupta <guptalucky3692@gmail.com>
2026-09-05 19:44:18 +0530

    ✅ Arrays [python3] — approach1

---
+++ solutions/python3/easy/np-arrays/solution-approach1.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/np-arrays/problem?isFullScreen=true
+# Problem     Arrays
+# Difficulty  Easy
+# Subdomain   Numpy
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-20, 01:47 p.m.
+# ──────────────────────────────────────────────────
+
+
+
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+    arr = numpy.array(arr,float)
+    return arr[::-1]
