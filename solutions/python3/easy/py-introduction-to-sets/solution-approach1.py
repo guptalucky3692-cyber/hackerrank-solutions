e0d03d141495cb8da19a006d297f9d82bcb3cad1
Lucky gupta <guptalucky3692@gmail.com>
2026-09-05 19:36:48 +0530

    ✅ Introduction to Sets [python3] — approach1

---
+++ solutions/python3/easy/py-introduction-to-sets/solution-approach1.py
@@ -0,0 +1,16 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/py-introduction-to-sets/problem?isFullScreen=true
+# Problem     Introduction to Sets
+# Difficulty  Easy
+# Subdomain   Sets
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-01, 10:16 p.m.
+# ──────────────────────────────────────────────────
+
+def average(array):
+    if len(arr)>n or len(arr)<n:
+        return false
+    return sum(set(arr))/len(set(arr))
+    # your code goes here
