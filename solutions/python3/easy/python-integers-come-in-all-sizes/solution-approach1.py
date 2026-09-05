80fb927710dac637878c93a805db8c6e64417ed0
Lucky gupta <guptalucky3692@gmail.com>
2026-09-05 19:42:03 +0530

    ✅ Integers Come In All Sizes [python3] — approach1

---
+++ solutions/python3/easy/python-integers-come-in-all-sizes/solution-approach1.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem?isFullScreen=true
+# Problem     Integers Come In All Sizes
+# Difficulty  Easy
+# Subdomain   Math
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-22, 02:31 p.m.
+# ──────────────────────────────────────────────────
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a =int(input())
+b =int(input())
+c =int(input())
+d =int(input())
+s = pow(a,b) + pow(c,d)
+print(s)
